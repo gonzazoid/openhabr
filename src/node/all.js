229e1ff5022a4b5eec6ad91b171a1d4f0ec30726
root <alloaolla@gmail.com>
2015-12-30 20:31:11 +0300

    start

--- src/node/all.js
+++ src/node/all.js
@@ -9,13 +9,16 @@ var pattern = fs.readFileSync("./tpl/all.tpl", "utf-8");
 
 var worker = function(request, response){
     pg.connect(config.common.postgres, function (err, pgClient, done) {
-        var sql = "SELECT * (SELECT * FROM articles WHERE draft != false ORDER BY stamp DESC LIMIT 10) ORDER BY stamp asc";
+        var sql = "SELECT * (SELECT * FROM articles WHERE draft != false ORDER BY stamp DESC LIMIT 10) ORDER BY stamp ASC;";
         pgClient.query({
             text: sql
 	   // ,values: argv
 	}, function(err, result){
             done();
 	    if(err){
+		console.log(err);
+                response.end();
+		return;
 	    }
             var headers = {};
 
