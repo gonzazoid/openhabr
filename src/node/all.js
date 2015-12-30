eb4eb2f3e139c140c2a2adc7469c0d7334df9624
root <alloaolla@gmail.com>
2015-12-30 18:05:29 +0300

    start

--- src/node/all.js
+++ src/node/all.js
@@ -3,6 +3,15 @@ var pg = require("pg");
 var mustach = require("mustach");
 
 var worker = function(request, response){
+        var headers = {};
+
+        headers['Content-Type'] = 'application/json';
+        headers['Expires'] = 'Mon, 26 Jul 1997 05:00:00 GMT'; //Дата в прошлом 
+        headers['Cache-Control'] = ' no-cache, must-revalidate'; // HTTP/1.1 
+        headers['Pragma'] = ' no-cache'; // HTTP/1.1 
+        //headers['Last-Modified'] = ".gmdate("D, d M Y H:i:s")."GMT");
+
+        response.writeHead(200, "Ok", headers);
     response.write("__hella!!!");
     response.end();
 };
