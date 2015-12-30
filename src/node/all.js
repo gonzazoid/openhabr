b67cb1dbbdcd32dda9a26938ca2adb762cdace28
root <alloaolla@gmail.com>
2015-12-30 18:38:04 +0300

    start

--- src/node/all.js
+++ src/node/all.js
@@ -8,7 +8,7 @@ var pattern = fs.readFileSync("./tpl/all.tpl");
 var worker = function(request, response){
         var headers = {};
 
-        headers['Content-Type'] = 'application/json';
+        headers['Content-Type'] = 'text/html';
         headers['Expires'] = 'Mon, 26 Jul 1997 05:00:00 GMT'; //Дата в прошлом 
         headers['Cache-Control'] = ' no-cache, must-revalidate'; // HTTP/1.1 
         headers['Pragma'] = ' no-cache'; // HTTP/1.1 
