bedce65b49940850533f6ff5929083ae9313c2d7
root <alloaolla@gmail.com>
2015-12-30 20:16:34 +0300

    start

--- src/node/all.js
+++ src/node/all.js
@@ -6,18 +6,30 @@ var mustache = require("mustache");
 var pattern = fs.readFileSync("./tpl/all.tpl", "utf-8");
 
 var worker = function(request, response){
-        var headers = {};
+    pg.connect(config.common.postgres, function (err, pgClient, done) {
+        var sql = "SELECT * (SELECT * FROM articles WHERE draft != false ORDER BY stamp DESC LIMIT 10) ORDER BY stamp asc";
+        pgClient.query({
+            text: sql,
+            values: argv
+	}, function(err, result){
+            done();
+	    if(err){
+	    }
+            var headers = {};
 
-        headers['Content-Type'] = 'text/html';
-        headers['Expires'] = 'Mon, 26 Jul 1997 05:00:00 GMT'; //Дата в прошлом 
-        headers['Cache-Control'] = ' no-cache, must-revalidate'; // HTTP/1.1 
-        headers['Pragma'] = ' no-cache'; // HTTP/1.1 
-        //headers['Last-Modified'] = ".gmdate("D, d M Y H:i:s")."GMT");
+            headers['Content-Type'] = 'text/html';
+            headers['Expires'] = 'Mon, 26 Jul 1997 05:00:00 GMT'; //Дата в прошлом 
+            headers['Cache-Control'] = ' no-cache, must-revalidate'; // HTTP/1.1 
+            headers['Pragma'] = ' no-cache'; // HTTP/1.1 
+            //headers['Last-Modified'] = ".gmdate("D, d M Y H:i:s")."GMT");
 
-        response.writeHead(200, "Ok", headers);
-    var output = mustache.render(pattern, {user: {name: "me"}});
-    response.write(output);
-    response.end();
+            response.writeHead(200, "Ok", headers);
+            var output = mustache.render(pattern, {user: {name: "me"}});
+            response.write(output);
+	    response.write(JSON.stringify(result.rows));
+            response.end();
+	});
+    });
 };
 
 http.createServer(worker).listen(7500, "localhost");
