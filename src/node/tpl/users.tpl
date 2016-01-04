1b0b57f3842c44b470745d78cd994617e8a2f5f9
root <alloaolla@gmail.com>
2016-01-04 14:17:18 +0300

    start

---
+++ src/node/tpl/users.tpl
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<title>{{article.title}}</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<link href="/css/post_common_css.css" rel="stylesheet" media="all" />
+<link href="/css/posts.css" rel="stylesheet" media="all" />
+<link href="/css/main.css" rel="stylesheet" media="all" />
+<style>
+.hub:after {
+    content: ","
+}
+.hub:last-of-type:after {
+    content: ""
+}
+</style>
+</head>
+<body>
+<div id="layout">
+<div id="navbar"></div>
+<div class="inner">
+
+  <div class="page_head">
+    <h2 class="title">Пользователи</h2>
+  </div>
+  <div class="content_left js-content_left">
+    <div class="peoples_list">
+      <div class="users_header">
+        <div class="rating">рейтинг</div>
+        <div class="karma">карма</div>
+      </div>
+      <div class="users peoples" id="peoples">
+{{#users}}
+        <div class="user " id="user_{{id}}">
+          <div class="lion_king"></div>
+          <div class="rating">{{rating}}</div>
+          <div class="karma">{{carma}}</div>
+          <div class="rating_change"></div>
+
+          <div class="avatar"><a href="/users/{{nickname}}/">
+        <span class="user-pic_default user-pic_default_47 user-pic_blue"></span>
+    </a>
+  </div>
+
+  <div class="info">
+    <div class="userlogin">
+        <div class="username"><a href="/users/{{nickname}}/">{{nickname}}</a></div>
+
+          <div class="buttons">
+            <input type="button" class="btn btn_subscribe  subscribeUser" value="Подписаться" data-id="{{id}}" data-login="{{nickname}}">
+            <input type="button" class="btn btn_subscribe btn_subscribed hidden unsubscribeUser" value="Отписаться" data-id="{{id}}" data-login="{{nickname}}">
+          </div>
+    </div>
+
+    <div class="lifetime">1 месяц на сайте</div>
+    <div class="last_post">последняя публикация: <a href="http://habrahabr.ru/post/273249/" class="grey">Как попасть на дачу президента в пять часов утра</a></div>
+
+
+  </div>
+  <div class="clear"></div>
+</div>
+{{/users}}
+
+      </div>
+    </div>
+  </div>
+{{>footer}}
+
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ src/node/users.js
@@ -0,0 +1,133 @@
+var http = require("http");
+var qs = require('querystring');
+var fs = require("fs");
+var pg = require("pg");
+var sha3 = require("js-sha3").sha3_512;
+var mustache = require("mustache");
+
+var config = require("./config");
+
+var pattern = fs.readFileSync("./tpl/user.tpl", "utf-8");
+var footer = fs.readFileSync("./tpl/footer.tpl", "utf-8");
+var users = fs.readFileSync("./tpl/users.tpl", "utf-8");
+
+var worker = function(request, response){
+    
+    //проверим action
+    //если newuser - пришли данные на регистрацию
+    console.log(request.post);
+    switch(request.url){
+        case "/":
+            //если нет никаких данных - просто выводим список пользователей
+            pg.connect(config.common.postgres, function (err, pgClient, done) {
+	        if(err){
+                    console.log(err);
+                    response.end();
+    	            return;
+	        }
+                var sql = "SELECT * FROM users ORDER BY carma DESC LIMIT 20;";   
+                pgClient.query({
+                    text: sql
+	            // ,values: argv
+	        }, function(err, result){
+                    done();
+	            if(err){
+		        console.log(err);
+                        response.end();
+		        return;
+	            }
+           
+                    var headers = {};
+
+                    headers['Content-Type'] = 'text/html';
+                    headers['Expires'] = 'Mon, 26 Jul 1997 05:00:00 GMT'; //Дата в прошлом 
+                    headers['Cache-Control'] = ' no-cache, must-revalidate'; // HTTP/1.1 
+                    headers['Pragma'] = ' no-cache'; // HTTP/1.1 
+                    //headers['Last-Modified'] = ".gmdate("D, d M Y H:i:s")."GMT");
+
+                    response.writeHead(200, "Ok", headers);
+                    var data = {users: result.rows};
+                    if("user" in request) data.user = request.user;
+                    var output = mustache.render(users, data, {footer: footer});
+                    response.write(output);
+                    //response.write(JSON.stringify(result.rows));
+                    response.end();
+                });
+            });
+            break;
+    }
+};
+var parseCookies = function (request) {//TODO audit&refactoring&error handling
+    //console.log(this.headers);
+    var cookies = {};
+
+    if (request.headers.cookie !== undefined) {
+        var rc = request.headers.cookie.split(';');
+
+        for (var cookie in rc) {
+            var parts = rc[cookie].split('=');
+            //TODO проверка на разбивку (должно быть точно два элемента)
+            if (parts.length !== 2) {
+                //this.forbidden();
+                console.log("wrong cookies");
+            }else{
+                cookies[parts[0].trim()] = parts[1].trim();
+            }
+        }
+    }
+    //console.log(this.cookies);
+    return cookies;
+};
+var start_session = function(request, response){
+    request.cookies = parseCookies(request); 
+    if("id" in request.cookies){
+        //тянем сессию
+        pg.connect(config.common.postgres, function (err, pgClient, done) {
+	    if(err){
+                console.log(err);
+                response.end();
+    	        return;
+	    }
+            var sql = "SELECT * FROM users WHERE sid = $1;"
+            pgClient.query({
+                text: sql
+	       ,values: [request.cookies.id]
+	    }, function(err, result){
+                done();
+	        if(err){
+		    console.log(err);
+                    response.end();
+		    return;
+	        } 
+                if(result.rows.length == 1){
+                    request.user = result.rows[0];
+                }
+                worker(request, response);
+            });
+        });
+    }
+    worker(request, response);
+};
+var starter = function (request, response) {
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+
+            if (body.length > 4096)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            request.post = qs.parse(body);
+            start_session(request, response);
+        });
+    }else{
+        request.post = {};
+        start_session(request, response);
+    }
+};
+
+http.createServer(starter).listen(7503, "localhost");
+console.log('users.server running at http://localhost:7503');
