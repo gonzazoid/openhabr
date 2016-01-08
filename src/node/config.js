0be9de75f31be96d2dc579b692a42cede1c9abe9
Timur <alloaolla@gmail.com>
2016-01-08 19:30:17 +0300

    Update config.js

--- src/node/config.js
+++ src/node/config.js
@@ -1,3 +1,4 @@
+//не пишите мне о том что я расшарил файл с паролем от базы если у вас нет четкого сценария как это использовать :)
 exports.common = {
         "postgres": "postgres://ordnung:atilla0453@localhost/habr",
     };
