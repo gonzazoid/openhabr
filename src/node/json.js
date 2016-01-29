92a40070b660109b623cf5c67a2a4516f12d78dc
root <alloaolla@gmail.com>
2016-01-29 17:27:26 +0300

    comments

---
+++ src/node/json.js
@@ -0,0 +1,23 @@
+"use strict";
+//просто что бы видеть, какие модули используются
+["http", "fs", "pg", "url", "mustache", "./bike", "./config"].forEach(cV => require(cV));
+
+var fs = require("fs");
+//var http = require("http");
+var app = require("express")();
+var fw = require("./bike");
+var cookies = require("cookie-parser");
+
+var patterns = {
+
+};
+
+app.use(fw.prepare_headers)
+   .use(cookies())
+   .use(fw.start_session)
+   .use(bodyParser.urlencoded({extended:true}))
+   .get("/comment/", function(req, res){
+        console.log(req);
+    })
+
+.listen(7506, "localhost", undefined,() => console.log('comment.server running at http://localhost:7506'));
