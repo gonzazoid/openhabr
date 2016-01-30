5d653ad3332a32cc05fcc9354e258dddfbf06a7e
root <alloaolla@gmail.com>
2016-01-30 16:42:18 +0300

    topics

--- src/node/users.js
+++ src/node/users.js
@@ -13,6 +13,7 @@ var patterns = {
    ,footer : fs.readFileSync("./tpl/footer.tpl", "utf-8")
    ,users  : fs.readFileSync("./tpl/users.tpl", "utf-8")
    ,user   : fs.readFileSync("./tpl/user.tpl", "utf-8")
+   ,user_topics : fs.readFileSync("./tpl/user_topics.tpl", "utf-8")
 };
 app.use(fw.prepare_headers)
    .use(cookies())
@@ -102,5 +103,54 @@ app.use(fw.prepare_headers)
 	          .end();
            });
        });
-   })
+    })
+    .get("^\/[a-zA-z0-9_\-]+\/topics\/$", function(req, res){
+        var pg = require("pg");
+        var config = require("./config");
+
+        pg.connect(config.common.postgres, function (err, pgClient, done) {
+            if(err){
+                console.log(err);
+                fw.err(res);
+                return;
+            }
+
+            var sql = "SELECT V.*,  M.hub_title, M.hub_id, M.hub_name, users.nickname FROM (    SELECT I.id, I.stamp, array_agg(J.title) as hub_title, array_agg(J.id) as hub_id, array_agg(J.name) as hub_name     FROM (        SELECT * FROM articles WHERE draft = false AND author=$1 ORDER BY stamp DESC LIMIT 10    ) I, hubs J     WHERE  J.id = ANY(I.hubs) GROUP BY I.id, I.stamp, I.hubs  ORDER BY I.stamp DESC) M, articles V, users WHERE M.id = V.id AND V.author = users.id ORDER BY stamp DESC;"
+        pgClient.query({
+            text: sql
+            values: [req.user.id]
+        }, function(err, result){
+            done();
+            if(err){
+	        console.log(err);
+                fw.err(res);
+	        return;
+	    }
+
+            //сольем вместе hub_title, id и name
+            result.rows.forEach(function(item, key, holder){
+                var hubs = [];
+                var i, l;
+                if(!item.hubs.length) return;
+                for(i=0, l=item.hub_id.length; i<l; i++){
+                    hubs[item.hub_id[i]] = {id: item.hub_id[i], name: item.hub_name[i], title: item.hub_title[i]};
+                }
+                for(i=0, l=item.hubs.length; i<l; i++){
+                    if(item.hubs[i] in hubs){
+                        item.hubs[i] = hubs[item.hubs[i]];
+                    }else{
+                        delete(item.hubs[i]);
+                    }
+                }
+            });
+            var mustache = require("mustache");
+	    var data = {};
+            data.articles = result.rows;
+            "user" in req && (data.user = req.user);
+	    res.status(200)
+	       .send(mustache.render(patterns.user_topics, data, patterns))
+	       .end();
+	});
+    });
+    })
 .listen(7503, "localhost", undefined,() => console.log('users.server running at http://localhost:7503'));
