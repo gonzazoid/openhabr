7452644e5e9012ef75dbdd3b65a01860efdbcac6
root <alloaolla@gmail.com>
2016-01-30 16:58:07 +0300

    topics

--- src/node/users.js
+++ src/node/users.js
@@ -109,48 +109,76 @@ app.use(fw.prepare_headers)
         var config = require("./config");
 
         pg.connect(config.common.postgres, function (err, pgClient, done) {
+            //{{{
             if(err){
                 console.log(err);
                 fw.err(res);
                 return;
             }
 
-            var sql = "SELECT V.*,  M.hub_title, M.hub_id, M.hub_name, users.nickname FROM (    SELECT I.id, I.stamp, array_agg(J.title) as hub_title, array_agg(J.id) as hub_id, array_agg(J.name) as hub_name     FROM (        SELECT * FROM articles WHERE draft = false AND author=$1 ORDER BY stamp DESC LIMIT 10    ) I, hubs J     WHERE  J.id = ANY(I.hubs) GROUP BY I.id, I.stamp, I.hubs  ORDER BY I.stamp DESC) M, articles V, users WHERE M.id = V.id AND V.author = users.id ORDER BY stamp DESC;"
-        pgClient.query({
-            text: sql
-            values: [req.user.id]
-        }, function(err, result){
-            done();
-            if(err){
-	        console.log(err);
-                fw.err(res);
-	        return;
-	    }
+            var result = req.url.split("/");
+            console.log(res);
+            var nickname = result[1];
+            var sql = "SELECT * FROM get_user_by_name($1);";   
+            pgClient.query({
+		//{{{
+                text: sql
+	       ,values: [nickname]
+	    }, function(err, result){
+                //done();
+	        if(err){
+	            done();
+	            console.log(err);
+                    fw.err(res);
+		    return;
+	        }   
+                if(result.rows.length != 1){
+                    //TODO что то пошло не так
+                }
 
-            //сольем вместе hub_title, id и name
-            result.rows.forEach(function(item, key, holder){
-                var hubs = [];
-                var i, l;
-                if(!item.hubs.length) return;
-                for(i=0, l=item.hub_id.length; i<l; i++){
-                    hubs[item.hub_id[i]] = {id: item.hub_id[i], name: item.hub_name[i], title: item.hub_title[i]};
-                }
-                for(i=0, l=item.hubs.length; i<l; i++){
-                    if(item.hubs[i] in hubs){
-                        item.hubs[i] = hubs[item.hubs[i]];
-                    }else{
-                        delete(item.hubs[i]);
-                    }
-                }
+
+
+                var sql = "SELECT V.*,  M.hub_title, M.hub_id, M.hub_name, users.nickname FROM (    SELECT I.id, I.stamp, array_agg(J.title) as hub_title, array_agg(J.id) as hub_id, array_agg(J.name) as hub_name     FROM (        SELECT * FROM articles WHERE draft = false AND author=$1 ORDER BY stamp DESC LIMIT 10    ) I, hubs J     WHERE  J.id = ANY(I.hubs) GROUP BY I.id, I.stamp, I.hubs  ORDER BY I.stamp DESC) M, articles V, users WHERE M.id = V.id AND V.author = users.id ORDER BY stamp DESC;"
+                pgClient.query({
+                    //{{{
+                    text: sql
+                    values: [result[0].id]
+                }, function(err, result){
+                    done();
+                    if(err){
+	                console.log(err);
+                        fw.err(res);
+	                return;
+	            }
+
+                    //сольем вместе hub_title, id и name
+                    result.rows.forEach(function(item, key, holder){
+                        var hubs = [];
+                        var i, l;
+                        if(!item.hubs.length) return;
+                        for(i=0, l=item.hub_id.length; i<l; i++){
+                            hubs[item.hub_id[i]] = {id: item.hub_id[i], name: item.hub_name[i], title: item.hub_title[i]};
+                        }
+                        for(i=0, l=item.hubs.length; i<l; i++){
+                            if(item.hubs[i] in hubs){
+                                item.hubs[i] = hubs[item.hubs[i]];
+                            }else{
+                                delete(item.hubs[i]);
+                            }
+                        }
+                    });
+                    var mustache = require("mustache");
+                    var data = {};
+                    data.articles = result.rows;
+                    "user" in req && (data.user = req.user);
+                    res.status(200)
+                       .send(mustache.render(patterns.user_topics, data, patterns))
+                       .end();
+                    //}}}
+	        });
+		    //}}}
             });
-            var mustache = require("mustache");
-	    var data = {};
-            data.articles = result.rows;
-            "user" in req && (data.user = req.user);
-	    res.status(200)
-	       .send(mustache.render(patterns.user_topics, data, patterns))
-	       .end();
-	});
-    });
+	    //}}}
+        });
     })
 .listen(7503, "localhost", undefined,() => console.log('users.server running at http://localhost:7503'));
