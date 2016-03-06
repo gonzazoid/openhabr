36be44f4771a927110e4ebe477f14a43c9c7eaa7
Sergey Ovechkin <me@pomeo.me>
2016-03-06 20:53:32 +0300

    refactoring posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,10 @@
-"use strict";
-//просто что бы видеть, какие модули используются
-["http", "fs", "pg", "url", "mustache", "querystring", "./bike", "./config"].forEach(cV => require(cV));
-
-var app = require("express")();
-//var http = require("http");
-var fs = require("fs");
-var fw = require("./bike");
-var cookies = require("cookie-parser");
-
-var patterns = {
-    posts:  fs.readFileSync("./tpl/posts.tpl", "utf-8")
-   ,footer: fs.readFileSync("./tpl/footer.tpl", "utf-8")
+export default (req, res) => {
+  // просто выводим форму редактора статьи
+  // TODO если пользователь не авторизован - предложить авторизоваться или зарегистрироваться
+  res.render('posts', {
+    user: req.user ? req.user : {},
+    partials: {
+      footer: 'footer'
+    }
+  });
 };
-
-app.use(fw.prepare_headers)
-   .use(cookies())
-   .use(fw.start_session)
-   .get("/add/", function(req, res){
-       //просто выводим форму редактора статьи
-       //TODO если пользователь не авторизован - предложить авторизоваться или зарегистрироваться
-       var mustache = require("mustache");
-       var data = {};
-       "user" in req && (data.user = req.user);
-       res.status(200)
-          .send(mustache.render(patterns.posts, data, patterns))
-          .end();
-    })
-.listen(7505, "localhost", undefined,() => console.log('posts.server running at http://localhost:7505'));
