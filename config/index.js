d2a1b8ef01c8db3f57da601d111f3dff5ed0d82e
Sergey Ovechkin <me@pomeo.me>
2016-03-06 21:38:13 +0300

    FIX typo

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 // не пишите мне о том что я расшарил файл с паролем от базы если у вас нет четкого сценария как это использовать :)
 const config = {
-  mongoURL: process.env.POSTGRES_URL || 'postgres://ordnung:atilla0453@localhost/habr',
+  postgresURL: process.env.POSTGRES_URL || 'postgres://ordnung:atilla0453@localhost/habr',
   ip: process.env.IP || '127.0.0.1',
   port: process.env.PORT || 7000
 };
