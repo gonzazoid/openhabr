43047b5af6937a0ae72de910d15bf6db6575614a
Sergey Ovechkin <me@pomeo.me>
2016-03-06 20:49:16 +0300

    rewrite config file

---
+++ config/index.js
@@ -0,0 +1,7 @@
+// не пишите мне о том что я расшарил файл с паролем от базы если у вас нет четкого сценария как это использовать :)
+const config = {
+  mongoURL: process.env.POSTGRES_URL || 'postgres://ordnung:atilla0453@localhost/habr',
+  port: process.env.PORT || 7000
+};
+
+export default config;
