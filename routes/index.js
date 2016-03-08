8c18746aabc17572ce2a73c38db4dee143963da7
Sergey Ovechkin <me@pomeo.me>
2016-03-08 11:05:49 +0300

    main routes file

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+const router = new Router();
+
+import Posts from './posts';
+import Auth from './auth';
+
+router.route('/').get();
+router.route('/post').get();
+router.route('/register').get();
+router.route('/users').get();
+router.route('/auth').get();
+router.route('/posts').get();
+router.route('/add').get(Posts);
+router.route('/logout').get(Auth);
+router.route('/json').get();
+
+export default router;
