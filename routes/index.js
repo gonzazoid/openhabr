d9ec4eccc49b0ccd28cfd3e973f1ab20fba10f09
Sergey Ovechkin <me@pomeo.me>
2016-03-10 10:25:44 +0300

    refactor register router

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 const router = new Router();
 
 import All from './all';
+import * as Reg from './register';
 import Posts from './posts';
 import Auth from './auth';
 
 router.route('/').get(All);
 router.route('/post').get();
-router.route('/register').get();
+router.route('/register').get(Reg.root);
+router.route('/register/newuser').post(Reg.newUser);
 router.route('/users').get();
 router.route('/auth').get();
 router.route('/posts').get();
--- routes/register.js
+++ routes/register.js
@@ -1,100 +1,85 @@
-"use strict";
-//просто что бы видеть, какие модули используются
-["http", "fs", "pg", "url", "mustache", "js-sha3", "validator", "querystring", "./bike", "./config"].forEach(cV => require(cV));
-var express = require("express");
-var app = express();
-var fs = require("fs");
-var fw = require("./bike");
-//var http = require("http");
-var cookies = require("cookie-parser");
-var bodyParser = require("body-parser");
+import pg        from 'pg-then';
+import config    from '../config';
+import validator from 'validator';
+import { sha3_512 as sha3 } from 'js-sha3';
 
-
-var patterns = {
-    register       : fs.readFileSync("./tpl/register.tpl", "utf-8")
-   ,footer         : fs.readFileSync("./tpl/footer.tpl", "utf-8")
-   ,congratulations: fs.readFileSync("./tpl/congratulations.tpl", "utf-8")
+const data = {};
+const rules = {
+  nickname: {
+    flags: 'required',
+    type: 'string'
+  },
+  sword: {
+    flags: 'required',
+    type: 'string'
+  },
+  mailbox: {
+    flags: 'required',
+    type: 'string'
+  },
+  promo: {
+    flags: 'notRequired',
+    type: 'string'
+  },
+  submit: {
+    flags: 'notRequired',
+    type: 'string'
+  }
 };
 
-var rndHex = function (len) {
-    var id = '';
-    var map = '0123456789abcdef';
+const pool = pg.Pool(config.postgresURL);
 
-    for (var $i = 0; $i < len; $i++) {
-        id += map.charAt(Math.floor(Math.random() * 16));
-    }
+export function root(req, res) {
+  data.user = req.session.user ? req.session.user : {};
+  data.partials = {
+    footer: 'footer'
+  };
+  res.render('register', data);
+}
 
-    return id;
-};
-app.use(fw.prepare_headers)
-   .use(cookies())
-   .use(bodyParser.urlencoded({extended:true}))
-   .get("/", function(req, res){
-       var mustache = require("mustache");
-       var data = {};
-       "user" in req && (data.user = req.user);
-       res.status(200)
-          .send(mustache.render(patterns.register, data, patterns))
-          .end();
-   })
-   .post("/newuser", function(req, res){
-       var mustache = require("mustache");
-       var pg = require("pg");
-       var config = require("./config");
-       var sha3 = require("js-sha3").sha3_512;
-       var validator = require("validator");
-       //регистрируем новичка
-       console.log(req.body);
-       var rules = {
-             "nickname" : {"flags": "required", "type": "string"}
-            ,"sword"    : {"flags": "required", "type": "string"}
-            ,"mailbox"  : {"flags": "required", "type": "string"}
-            ,"promo"    : {"flags": "notRequired", "type": "string"}
-            ,"submit"   : {"flags": "notRequired", "type": "string"}
-       };
-       try {
-           req.post = validator(req.body, rules);
-       } catch (err) {
-           console.log("register: bad request:\nuser:\n", req.user, "\npost:\n", req.body, "\nerror\n", err);
-           res.sendStatus(500)
-	      .end();
-           return;
-       }
-       pg.connect(config.common.postgres, function (err, pgClient, done) {
-           if(err){
-               console.log(err);
-               res.sendStatus(500)
-	          .end();
-               return;
-           }
-           var sql = "select * from adduser($1, $2, $3, $4);"
-           var sid = rndHex(128);
-           pgClient.query({
-               text: sql
-              ,values: [req.post.nickname, req.post.mailbox, sha3(req.post.sword), sid]
-           }, function(err, result){
-               done();
-               if(err){
-                   res.sendStatus(500)
-                      .end();
-                   return;
-               }
-               console.log(result);
-	       // если все хорошо - выставляем куки и поздравляем с регистрацией
-               var data = {};
-               if(result.rows[0].adduser){
-                   //куки авторизации
-                   res.set("Set-Cookie", 'id=' + sid + '; path=/; HttpOnly;');
-                   data.user = {nickname: req.post.nickname};
-                   res.status(200)
-                      .send(mustache.render(patterns.congratulations, data, patterns))
-                      .end();
-               }else{//что то пошло не так - просим повторить заново (логин либо почта уже есть в системе)
-                   res.status(200)
-                      .send(mustache.render(patterns.register, data, patterns))
-                      .end();
-               }
-           });
-       });
-   })
-.listen(7502, "localhost", undefined, () => console.log('views.server running at http://localhost:7502'));
+export function newUser(req, res) {
+  console.log(req.body);
+  let post;
+
+  try {
+    post = validator(req.body, rules);
+  } catch (err) {
+    console.error(`
+register: bad request:
+user:
+${req.user}
+post:
+${req.body}
+error
+${err}
+`);
+    res.sendStatus(500).end();
+  }
+
+  const sql = 'select * from adduser($1, $2, $3, $4);';
+
+  pool
+    .query({
+      text: sql,
+      values: [post.nickname, post.mailbox, sha3(post.sword), '']
+    })
+    .then(result => {
+      console.log(result);
+      data.user = {
+        nickname: post.nickname
+      };
+      data.partials = {
+        footer: 'footer'
+      };
+      if (result.rows[0].adduser) {
+        req.session.user = post.nickname;
+        res.render('congratulations', data);
+      } else {
+        // что то пошло не так - просим повторить заново (логин либо почта уже есть в системе)
+        res.render('register', data);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
